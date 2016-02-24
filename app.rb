6b91da94bee8596b50d56f65e47196f4c1780eef
lcostantini <costantinileandro1@gmail.com>
2016-02-24 16:27:11 -0300

    Add styles for the views

--- app.rb
+++ app.rb
@@ -4,16 +4,17 @@ require './models/pg_post'
 require './models/redis_post'
 
 Cuba.plugin Cuba::Render
+Cuba.use Rack::Static, urls: ['/css'], root: 'public'
 
 Cuba.define do
   on get do
     on root do
-      res.write partial "index"
+     render 'index'
     end
 
     on 'redis' do
       on root do
-
+        render 'redis', posts: RedisPost.all.to_a
       end
 
       on params('q') do |query|
@@ -23,7 +24,7 @@ Cuba.define do
 
     on 'postgres' do
       on root do
-
+        render 'postgres', posts: PgPost.all
       end
 
       on params('q') do |query|
