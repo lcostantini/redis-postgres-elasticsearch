5e149cdcc20a69b5e7c8d9e6bf2d8a04839af994
lcostantini <costantinileandro1@gmail.com>
2016-02-24 16:27:30 -0300

    Add model connection in ActiveRecord.

--- models/pg_post.rb
+++ models/pg_post.rb
@@ -1,4 +1,6 @@
 require 'active_record'
 
 class PgPost < ActiveRecord::Base
+    connection_details = YAML::load(File.open('config/database.yml'))
+    ActiveRecord::Base.establish_connection(connection_details)
 end
