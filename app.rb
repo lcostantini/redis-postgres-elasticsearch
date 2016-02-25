ab99fc9d90a899b228bf9f0aa854b37feb94c0fe
lcostantini <costantinileandro1@gmail.com>
2016-02-25 16:05:52 -0300

    Add search box.

--- app.rb
+++ app.rb
@@ -12,6 +12,22 @@ Cuba.define do
      render 'index', posts: PgPost.all, total: nil, elapsed_time: nil
     end
 
+    on 'search', param('q'), param('db') do |query, db|
+      case db
+      when 'in Redis'
+        before = Time.now
+        posts = RedisPost.find(author: query.upcase).to_a
+        after = Time.now
+      when 'in Postgres'
+        before = Time.now
+        posts = PgPost.where("author = ?", query.upcase)
+        after = Time.now
+      else
+        res.write 'Elasticsearch'
+      end
+      render 'index', posts: posts, total: posts.count, elapsed_time: after - before
+    end
+
     on 'redis' do
       on root do
         before = Time.now
